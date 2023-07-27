d3ed732488148ad4fc56f6d6a5aa661187d78002
EDUARDO COSTA DA SILVA <eduardo.csilva52@senacsp.edu.br>
2023-07-27 17:12:52 -0300

    calculadora

---
+++ CalculadoraCerto1/ModelCalculadora.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraCerto1
+{
+
+    class ModelCalculadora
+    {
+        //Declarando variáveis
+        private double num1;
+        private double num2;
+
+        public ModelCalculadora()
+        {
+            GetSetNum1 = 0;
+            GetSetNum2 = 0;
+        }//fim do construtor
+
+        //Métodos Gets e Sets
+        public double GetSetNum1
+        {
+            get { return this.num1; }
+            set { this.num1 = value; }
+        }//fim do GetSetNum1
+
+        public double GetSetNum2
+        {
+            get { return this.num2; }
+            set { this.num2 = value; }
+        }//fim do GetSetNum2
+
+        //Métodos
+        public double Somar()
+        {
+            return GetSetNum1 + GetSetNum2;
+        }//fim do método somar
+
+        public double Subtrair()
+        {
+            return GetSetNum1 - GetSetNum2;
+        }//fim do método subtrair
+
+        public double Dividir()
+        {
+            if (GetSetNum2 <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return GetSetNum1 / GetSetNum2;
+            }
+        }//fim do método dividir
+
+        public double Multiplicar()
+        {
+            return GetSetNum1 * GetSetNum2;
+        }//fim do método multiplicar
+
+        public double Potencia()
+        {
+            return Math.Pow(GetSetNum1, GetSetNum2);
+        }//fim da potência
+
+        public string Raiz()
+        {
+            string msg = "Raiz do primeiro número: " + Math.Sqrt(GetSetNum1) +
+                         "\nRaiz do segundo número: " + Math.Sqrt(GetSetNum2);
+            return msg;
+        }//fim do método raiz
+
+        public string TabuadaNum1()
+        {
+            string resultado = "";
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado += "\n" + GetSetNum1 + " * " + i + " = " + (GetSetNum1 * i);
+            }//fim do for
+
+            return resultado;
+        }//fim do método
+
+        public string TabuadaNum2()
+        {
+            string resultado = "";
+
+            //Início;Fim;Contagem
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado += "\n" + GetSetNum2 + " * " + i + " = " + (GetSetNum2 * i);
+            }//fim do for
+
+            return resultado;
+        }//fim do método
+
+        public double ConverterDecimal(string binario)
+        {
+            double dec = 0;
+            string caract = "";
+            int tamanho = binario.Length;
+            int pote = tamanho;
+            for (int i = 0; i < tamanho; i++)
+            {
+                pote--;
+                caract = binario.Substring(i, 1);
+                if (caract == "1")
+                {
+                    dec += Math.Pow(2, pote);
+                }
+            }//fim do for
+            return dec;
+        }//fim do método converter decimal
+
+        public string ConverterBinario(int dec)
+        {
+            string binario = "";
+            int resto = 0;
+            do
+            {
+                resto = dec % 2;
+                binario += resto;
+                dec = dec / 2;
+            } while (dec != 0);
+            return new String(binario.Reverse().ToArray());
+        }//fim do método
+
+        public double ConverterHexaDecimal(string hexadecimal)
+        {
+            double dec = 0;
+            string caract = "";
+            int num = 0;
+            int tamanho = hexadecimal.Length;
+            int pote = tamanho;
+            for (int i = 0; i < tamanho; i++)
+            {
+                pote--;
+                caract = hexadecimal.Substring(i, 1);
+                switch (caract)
+                {
+                    case "A":
+                        num = 10;
+                        break;
+                    case "B":
+                        num = 11;
+                        break;
+                    case "C":
+                        num = 12;
+                        break;
+                    case "D":
+                        num = 13;
+                        break;
+                    case "E":
+                        num = 14;
+                        break;
+                    case "F":
+                        num = 15;
+                        break;
+                    default:
+                        num = Convert.ToInt32(caract);
+                        break;
+                }//fim do switch
+                dec += num * Math.Pow(16, pote);
+            }//fim do for
+            return dec;
+        }//fim do método converter decimal
+
+        public string ConverterDecimalHexa(int dec)
+        {
+            int resto = 0;
+            string hexadecimal = "";
+
+            do
+            {
+                resto = dec % 16;
+                switch (resto)
+                {
+                    case 10:
+                        hexadecimal += "A";
+                        break;
+                    case 11:
+                        hexadecimal += "B";
+                        break;
+                    case 12:
+                        hexadecimal += "C";
+                        break;
+                    case 13:
+                        hexadecimal += "D";
+                        break;
+                    case 14:
+                        hexadecimal += "E";
+                        break;
+                    case 15:
+                        hexadecimal += "F";
+                        break;
+                    default:
+                        hexadecimal += resto;
+                        break;
+                }//fim do switch
+                dec = dec / 16;
+            } while (dec != 0);
+            return new string(hexadecimal.Reverse().ToArray());
+        }//fim do converter
+
+
+
+        public string bhaskara(double a, double b, double c)
+        {
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            if (delta <= 0)
+            {
+                return "Impossível calcular X1 e X2, Delta: " + delta;
+            }
+            else
+            {
+                double X1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double X2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                return "Delta: " + delta + "\nX1: " + X1 + "\nX2: " + X2;
+            }
+        }//fim do bhaskara
+
+
+
+
+
+
+
+
+
+
+
+
+        //exercicio numero 1
+
+
+
+        public string DobroeTriplo(double d, double e)
+        {
+
+            GetSetNum1 = d * +2;
+            GetSetNum2 = e * +3;
+
+            string msg = "o dobro e " + d + " e o triplo e" + e;
+            return msg;
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+        //exercicio 3
+
+
+        public string Numerospares()
+        {
+            if (GetSetNum1 <= 0)
+            {
+                return "numero negativo" + GetSetNum1;
+
+
+            }
+
+            else
+
+            {
+                return "numero positivo  " + GetSetNum1;
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+        //exercicio 8
+
+
+
+
+
+        public double NumerosContar(double a)
+        {
+            a = GetSetNum1;
+            double contador = GetSetNum1;
+
+            do
+            {
+                Console.WriteLine("Contagem: " + GetSetNum1 *10);
+                GetSetNum1 ++;
+                
+            } while (GetSetNum1 <= 10);
+
+
+
+
+
+
+
+
+            return a * 10;
+                
+                
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//fim da classe
+
---
+++ CalculadoraCerto1/control calculadora.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraCerto1
+{
+
+    class ControlCalculadora
+    {
+        //Criando um objeto calculadora na memória
+        ModelCalculadora calculadora;
+
+        public ControlCalculadora()
+        {
+            this.calculadora = new ModelCalculadora();
+        }//fim do construtor
+
+        public void Coletar()
+        {
+            Console.WriteLine("Informe um número: ");
+            this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe outro número: ");
+            this.calculadora.GetSetNum2 = Convert.ToDouble(Console.ReadLine());
+        }//fim do coletar
+
+        public void Pegarnumero()
+        {
+
+            Console.WriteLine("Informe um número: ");
+            this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public int Menu()
+        {
+            Console.WriteLine("------ Menu -------" +
+                              "\n0. Sair" +
+                              "\n1. Somar" +
+                              "\n2. Subtrair" +
+                              "\n3. Dividir" +
+                              "\n4. Multiplicar" +
+                              "\n5. Potência" +
+                              "\n6. Raiz" +
+                              "\n7. Tabuada" +
+                              "\n8. Conversão de binário para decimal" +
+                              "\n9. Conversão de decimal para binário" +
+                              "\n10. Hexadecimal para decimal " +
+                              "\n11. Decimal para Hexadecimal" +
+                              "\n12. Bhaskara" +
+                              "\n13. convertendo de binario para exa" +
+                              "\n14. dobro e triplo" +
+                              "\n15 positivo" +
+                              "\n16.contar" +
+
+                              "\n\nEscolha uma das opções acima: ");
+
+
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            return opcao;
+        }//fim do método Menu
+
+        public void Operacao()
+        {
+            int opcao = 0;
+            do
+            {
+                opcao = Menu();
+                Console.Clear();//Limpa a tela
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    case 1:
+                        Coletar();
+                        Console.WriteLine("Soma: " + this.calculadora.Somar());
+                        break;
+                    case 2:
+                        Coletar();
+                        Console.WriteLine("Subtração: " + this.calculadora.Subtrair());
+                        break;
+                    case 3:
+                        Coletar();
+                        if (this.calculadora.Dividir() == -1)
+                        {
+                            Console.WriteLine("Impossível dividir!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Divisão: " + this.calculadora.Dividir());
+                        }
+                        break;
+                    case 4:
+                        Coletar();
+                        Console.WriteLine("Multiplicar: " + this.calculadora.Multiplicar());
+                        break;
+                    case 5:
+                        Coletar();
+                        Console.WriteLine("Potência: " + this.calculadora.Potencia());
+                        break;
+                    case 6:
+                        Coletar();
+                        Console.WriteLine(this.calculadora.Raiz());
+                        break;
+                    case 7:
+                        Console.WriteLine("Informe um número: ");
+                        this.calculadora.GetSetNum1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(this.calculadora.TabuadaNum1());
+                        break;
+                    case 8:
+                        Console.WriteLine("Informe um valor em binário: ");
+                        Console.WriteLine(this.calculadora.ConverterDecimal(Console.ReadLine()));
+                        break;
+                    case 9:
+                        Console.WriteLine("Informe um valor em decimal: ");
+                        Console.WriteLine(this.calculadora.ConverterBinario(Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    case 10:
+                        Console.WriteLine("Informe um valor em Hexadecimal: ");
+                        Console.WriteLine(this.calculadora.ConverterHexaDecimal(Console.ReadLine()));
+                        break;
+                    case 11:
+                        Console.WriteLine("Informe um valor em Decimal: ");
+                        Console.WriteLine(this.calculadora.ConverterDecimalHexa(Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    case 12:
+                        Console.WriteLine("Informe A: ");
+                        double a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe B: ");
+                        double b = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe C: ");
+                        double c = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(this.calculadora.bhaskara(a, b, c));
+                        break;
+                    default:
+                        Console.WriteLine("Opção escolhida não é valida");
+                        break;
+                    case 13:
+                        Console.WriteLine("informe um valor binario");
+                        string binario = Console.ReadLine();
+                        Console.WriteLine(this.calculadora.ConverterHexaDecimal(binario));
+                        break;
+                    case 14:
+
+                        Console.WriteLine("Informe b: ");
+                        double d = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe e: ");
+                        double e = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(this.calculadora.DobroeTriplo(d, e));
+                        break;
+
+
+                    case 15:
+                        Pegarnumero();
+                        Console.WriteLine("informe um numero: ");
+                        Console.WriteLine(this.calculadora.Numerospares());
+                        break;
+
+                    case 16:
+                        Pegarnumero();
+                        Console.WriteLine("informe um numero: ");
+                        double a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(this.calculadora.NumerosContar(a));
+                        break;
+
+
+
+
+
+
+
+                }//fim do switch
+
+            } while (opcao != 0);//fim do Do...while
+        }//fim do operacao
+    }//fim da classe
+}//fim do projeto
+
+
